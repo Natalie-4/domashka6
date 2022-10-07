407ec4446dc5130a17ebe98558ff1fb206371ce2
Natalie <itlaym@yandex.ru>
2022-10-07 23:43:19 +0700

    solve task_43

--- task_43/Program.cs
+++ task_43/Program.cs
@@ -3,4 +3,18 @@ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задают
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите угловой коэффициент первой прямой k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите свободный коэффициент первой прямой b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите угловой коэффициент второй прямой k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите свободный коэффициент первой прямой b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+int x = (b2-b1)/(k1-k2);
+int y = k1*x + b1;
+
+Console.WriteLine("Точка пересечения двух прямых: (" + x + "," + y +")");
